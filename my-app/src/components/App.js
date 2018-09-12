624eb84e0e20ae547640fda7a20879bc17c5d973
gektor94 <skoverin@gmail.com>
2018-09-12 11:45:55 +0300

    first commit

---
+++ my-app/src/components/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Article from './Article'
+import articles from '../fixtures'
+ 
+function App() {
+    return (
+        <div>
+            <h1>App Name</h1>
+            <Article article={articles[1]} />
+        </div>
+    )
+}
+
+export default App
